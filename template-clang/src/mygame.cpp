3e0622fe2ed7d80f373011012a663c1ac84b3517
gmh5225 <2315157@qq.com>
2023-02-04 03:46:35 +0800

    template-clang

---
+++ template-clang/src/mygame.cpp
@@ -0,0 +1,27 @@
+//#define YOURGAME_GL_INCLUDE_GLES1
+//#include "yourgame/yourgame.h"
+//#include "mygame_version.h"
+
+//namespace yg = yourgame; // convenience
+
+namespace mygame
+{
+    
+
+    void init(int argc, char *argv[])
+    {
+       
+    }
+
+    void tick()
+    {
+      
+    }
+
+    int shutdown()
+    {
+      
+        return 0;
+    }
+
+} // namespace mygame
---
+++ template-clang/src/version.cpp
@@ -0,0 +1,44 @@
+/*
+Copyright (c) 2019-2022 Alexander Scholz
+
+This software is provided 'as-is', without any express or implied
+warranty. In no event will the authors be held liable for any damages
+arising from the use of this software.
+
+Permission is granted to anyone to use this software for any purpose,
+including commercial applications, and to alter it and redistribute it
+freely, subject to the following restrictions:
+
+1. The origin of this software must not be misrepresented; you must not
+   claim that you wrote the original software. If you use this software
+   in a product, an acknowledgment in the product documentation would be
+   appreciated but is not required.
+2. Altered source versions must be plainly marked as such, and must not be
+   misrepresented as being the original software.
+3. This notice may not be removed or altered from any source distribution.
+*/
+#include "yourgame/version.h"
+
+namespace yourgame
+{
+    namespace version
+    {
+        const int major = 0;
+        const int minor = 0;
+        const int patch = 0;
+        const std::string git_commit = "2b1f7672ee18c2ce6aafe8e1c588c5a1a40cd091-dirty";
+        const std::string git_date = "2022-09-26 19:58:59 +0200";
+        const std::string PROJECT_NAME = "yourgame";
+        const std::string CMAKE_SYSTEM = "Windows-10.0.22000";
+        const std::string CMAKE_SYSTEM_PROCESSOR = "AMD64";
+        const std::string CMAKE_HOST_SYSTEM = "Windows-10.0.22000";
+        const std::string CMAKE_HOST_SYSTEM_PROCESSOR = "AMD64";
+        const std::string CMAKE_C_COMPILER_ID = "MSVC";
+        const std::string CMAKE_C_COMPILER_VERSION = "19.33.31630.0";
+        const std::string CMAKE_CXX_COMPILER_ID = "MSVC";
+        const std::string CMAKE_CXX_COMPILER_VERSION = "19.33.31630.0";
+        const std::string CMAKE_GENERATOR = "Visual Studio 17 2022";
+        const std::string CMAKE_GENERATOR_PLATFORM = "x64";
+        const int CMAKE_SIZEOF_VOID_P = 8;
+    }
+} // namespace yourgame
